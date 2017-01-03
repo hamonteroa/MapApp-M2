b751a6a9efc2520b62d4c7d6b90d2a2f32f79191
Hector Montero <hamonteroa@Hectors-MacBook-Pro.local>
2017-01-03 10:17:33 -0500

    Initial Commit

---
+++ MapApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MapApp
+//
+//  Created by Hector Montero on 1/3/17.
+//  Copyright © 2017 Hector Montero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
